514e07e439f8193439a33f3e5a6f6e00725f32f8
cuichuanwa_v <cuichuanwa_v@didiglobal.com>
2021-03-18 16:59:16 +0800

    update

---
+++ vue_vux_vuerouter/src/directive/Test.js
@@ -0,0 +1,8 @@
+Vue.directive("reload-img", {
+    bind: function(el, binding) {
+      el.src = binding.value ? binding.value : "/verifyCode.api";
+      el.addEventListener("click", function(event) {
+        el.src = el.src + "?t=" + new Date().getTime();
+      });
+    }
+  });
